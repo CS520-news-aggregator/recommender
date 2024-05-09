3d60972d5357036e0a2dae2bb96369132a2463ed
Ji Cao <caoji2002@hotmail.com>
2024-05-09 14:59:11 -0400

    Finished recommender testing

--- tests/test_recommender.py
+++ tests/test_recommender.py
@@ -1,33 +1,28 @@
 from fastapi.testclient import TestClient
 from fastapi.encoders import jsonable_encoder
-from models.pub_sub import Subscriber
+from models.recommendation import RecommendationQuery
 from main import app
 
 client = TestClient(app)
 
 
-# TODO change tests
-def test_post_sub():
-    sub = Subscriber(ip_address="123456789", port=1000)
-    response = client.post("/observer/subscribe", json=jsonable_encoder(sub))
-    assert True
+def test_get_recommendations():
+    response = client.get("/recommender/get-recommendations", params={"user_id": "baduserid", "limit": 10, "page": 2})
+    assert response.status_code == 404
+
+    response = client.get(
+        "/recommender/get-recommendations", params={"user_id": "01f98fe7-f86f-4f62-8440-5ed90ac3b502", "limit": 10, "page": 2}
+    )
+    assert response.status_code == 200
+    assert (
+        "message" in response.json()
+        and "list_recommendations" in response.json()
+        and response.json()["message"] == "Recommendation sent"
+    )
 
 
-#     assert response.status_code == 200
-#     assert response.json() == {"message": "Observer subscribed"}
-
-#     sub2 = Subscriber(ip_address="123456789", port=1000)
-#     response = client.post("/observer/subscribe", json=jsonable_encoder(sub2))
-#     assert response.status_code == 400
-
-
-# def test_get_sub():
-#     response = client.get("/observer/subscribers")
-#     assert response.status_code == 200
-#     assert "subscribers" in response.json() and type(response.json()["subscribers"]) is list
-
-
-# def test_update_sub_fail(capsys):
-#     update_subscribers(["random"])
-#     captured = capsys.readouterr()
-#     assert "Could not send update to" in captured.out
+def test_post_recommendations():
+    rq = RecommendationQuery(post_ids=["cf3c5b6c-4ac0-4f8f-9195-575dbd463f95"])
+    response = client.post("/recommender/add-recommendations", json=jsonable_encoder(rq))
+    assert response.status_code == 200
+    assert response.json() == {"message": "Recommendation process started"}
